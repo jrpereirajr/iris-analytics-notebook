7f39f17562d441dfc125392da68cb6646436a858
José <joseroberto.pereira@shift.com.bt>
2020-11-28 10:28:04 -0300

    minimum setup for IRIS unit teste, Angular unit test and ZPM

---
+++ frontend/src/app/notebook/notebook.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotebookComponent } from './notebook/notebook.component';
+
+
+
+@NgModule({
+  declarations: [NotebookComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class NotebookModule { }
