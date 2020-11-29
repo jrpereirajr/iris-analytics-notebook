8ba3d8333efa7e0b8186b1f16b821c2186772839
José <joseroberto.pereira@shift.com.bt>
2020-11-29 11:14:26 -0300

    minimum setup for IRIS unit teste, Angular unit test and ZPM

--- frontend/src/app/notebook/notebook/notebook.component.spec.ts
+++ frontend/src/app/notebook/notebook/notebook.component.spec.ts
@@ -1,34 +1,75 @@
+import { Component, ViewChild } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { NotebookInterface } from '../notebook.models';
+import { NotebookModule } from '../notebook.module';
 import { NotebookComponent } from './notebook.component';
 
-fdescribe('NotebookComponent', () => {
-  let component: NotebookComponent;
-  let fixture: ComponentFixture<NotebookComponent>;
+/**
+ * @see https://jenniferwadella.com/blog/testing-forms-using-CVA
+ */
+@Component({
+  template: `
+  <form [formGroup]="notebookForm">
+    <app-notebook formControlName="notebook"></app-notebook>
+  </form>
+  `
+})
+class TestHostComponent {
+  @ViewChild(NotebookComponent, { static: true })
+  public notebookComponent: NotebookComponent;
 
-  const notebooks: NotebookInterface = {
-    name: 'Notebook1'
-  };
+  public testData = null;
+  public notebookForm = new FormGroup({
+    notebook: new FormControl({ value: this.testData, disabled: true })
+  });
+}
+
+fdescribe('NotebookComponent', () => {
+  let hostFixture: ComponentFixture<TestHostComponent>;
+  let testHostComponent: TestHostComponent;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NotebookComponent ],
+      declarations: [NotebookComponent, TestHostComponent],
       imports: [
+        ReactiveFormsModule,
         TranslateModule.forRoot()
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NotebookComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostFixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = hostFixture.componentInstance;
+    hostFixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(testHostComponent.notebookComponent).toBeTruthy();
   });
 
   it('should create with null value', () => {
-    expect(component).toBeTruthy();
+    const compiled = hostFixture.debugElement.nativeElement;
+    const component = compiled.querySelector('app-notebook');
+    const content = [...component.querySelectorAll('div')];
+    expect(content.length).toEqual(1);
+    expect(content[0].innerText).toEqual('notebook.noNotebook');
+  });
+
+  it('should create with value', () => {
+    const data = { name: 'foo' };
+    testHostComponent.notebookForm.patchValue({ notebook: data });
+    hostFixture.detectChanges();
+    const compiled = hostFixture.debugElement.nativeElement;
+    const component = compiled.querySelector('app-notebook');
+    const content = [...component.querySelectorAll('div')];
+
+    expect(testHostComponent.notebookForm.get('notebook').value).toBe(data);
+    expect(content.length).toEqual(1);
+    expect(content[0].innerText).toEqual(data.name);
   });
 });
